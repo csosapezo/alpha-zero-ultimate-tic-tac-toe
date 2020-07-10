f577561d0138ceccd1b056034820ad33d4c3ef59
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-07-10 15:52:25 -0500

    partial adaptation

--- ultimate_tictactoe/UltimateTicTacToePlayers.py
+++ ultimate_tictactoe/UltimateTicTacToePlayers.py
@@ -1,3 +1,5 @@
+from random import random
+
 import numpy as np
 
 
@@ -11,6 +13,73 @@ class RandomUltimateTictacToePlayer():
         while valids[a] != 1:
             a = np.random.randint(self.game.getActionSize())
         return a
+
+
+def ucb(n, C=1.4):
+    if n.N == 0:
+        return np.inf
+    else:
+        return (n.U / n.N) + C * np.sqrt(np.log(n.parent.N) / n.N)
+
+
+class MCT_Node:
+    """Node in the Monte Carlo search tree, keeps track of the children states."""
+
+    def __init__(self, parent=None, state=None, U=0, N=0):
+        self.__dict__.update(parent=parent, state=state, U=U, N=N)
+        self.children = {}
+        self.actions = None
+
+
+class MonteCarloTreeSearchPlayer:
+    def __init__(self, n=25):
+        self.n = n
+
+    def play(self, state, game):
+        def select(n):
+            """select a leaf node in the tree"""
+            if n.children:
+                return select(max(n.children.keys(), key=ucb))
+            else:
+                return n
+
+        def expand(n):
+            """expand the leaf node by adding all its children states"""
+            if not n.children and not game.getGameEnded(game, 1):
+                n.children = {MCT_Node(state=game.getNextState(n.state, action)[0], parent=n): action
+                              for action in game.getValidMoves(n.state, 1)}
+            return select(n)
+
+        def simulate(game, state):
+            """simulate the utility of current state by random picking a step"""
+            player = game.to_move(state)
+            while not game.terminal_test(state):
+                action = random.choice(list(game.actions(state)))
+                state = game.result(state, action)
+            v = game.utility(state, player)
+            return -v
+
+        def backprop(n, utility):
+            """passing the utility back to all parent nodes"""
+            if utility > 0:
+                n.U += utility
+            # if utility == 0:
+            #     n.U += 0.5
+            n.N += 1
+            if n.parent:
+                backprop(n.parent, -utility)
+
+        root = MCT_Node(state=state)
+
+        for _ in range(self.n):
+            leaf = select(root)
+            child = expand(leaf)
+            result = simulate(game, child.state)
+            backprop(child, result)
+
+        max_state = max(root.children, key=lambda p: p.N)
+
+        return root.children.get(max_state)
 
 
 class HumanUltimateTicTacToePlayer():
